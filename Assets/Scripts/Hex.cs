ecca6b693fa67d25186965da62f0c990d46cbab9
asRagi <ragi-justaway@hotmail.co.jp>
2019-03-12 01:53:10 +0900

    interact

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Hex : MonoBehaviour
 {
+    HexMaster master;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,23 @@ public class Hex : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void OnMouseOver()
+    {
+        Contacted();
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void Contacted()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    public void SetHexMaster(HexMaster hm) => master = hm;
 }
