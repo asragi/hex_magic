20625f96a2ea174a565cac5d5a5799c5107a7330
asRagi <ragi-justaway@hotmail.co.jp>
2019-03-13 03:10:21 +0900

    next

--- Assets/Scripts/HexNext.cs
+++ Assets/Scripts/HexNext.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HexNext : MonoBehaviour
 {
+    [SerializeField]
+    HexPairObj hexPairNext;
+    [SerializeField]
+    HexPairObj hexPairNext2;
     HexColorPair next, next2;
     HexDeck deck;
     // Start is called before the first frame update
@@ -13,12 +17,19 @@ public class HexNext : MonoBehaviour
         deck.MakeDeck();
         next = new HexColorPair(deck.Pop(), deck.Pop());
         next2 = new HexColorPair(deck.Pop(), deck.Pop());
+        Refresh();
+    }
+
+    private void Refresh(){
+        hexPairNext.ChangeHex(next);
+        hexPairNext2.ChangeHex(next2);
     }
 
     public HexColorPair Pop(){
         var tmp = next;
         next = next2;
         next2 = new HexColorPair(deck.Pop(), deck.Pop());
+        Refresh();
         return tmp;
     }
 }
